99034eaa4a2ac51819f26bc2dccd3dbd25e57077
XRonaldXD <shamhoikin0308@gmail.com>
2025-10-30 23:35:38 +0800

    added oauth2 for google

--- index.js
+++ index.js
@@ -1,7 +1,49 @@
+require('dotenv').config();
+require('./config/passport'); // Import Passport configuration
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const path = require('path');
 const port = require('./config/database').port;
+const app = express();
 
-app.listen(port, () => {
-    console.log(`Express app listening on port ${port}`);
+// session middleware
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}));
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/auth', require('./routes/auth'));
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'login.html'));
 });
+
+app.get('/profile', isLoggedIn, (req, res) => {
+    res.sendFile(path.join(__dirname, 'view', 'profile.html'));
+});
+
+mongoose.connect(process.env.URL)
+    .then(() => {
+        console.log('MongoDB connected');
+        app.listen(port, () => {
+            console.log(`Express app listening on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('MongoDB connection error:', err);
+    });
+
+
+function isLoggedIn(req, res, next) {
+    req.user ? next() : res.sendStatus(401);
+}
