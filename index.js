28a24e17192416de3eec9eee33d9071741a9f13a
XRonaldXD <shamhoikin0308@gmail.com>
2025-10-30 22:18:43 +0800

    init folders

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = require('./config/database').port;
 
 app.listen(port, () => {
     console.log(`Express app listening on port ${port}`);
